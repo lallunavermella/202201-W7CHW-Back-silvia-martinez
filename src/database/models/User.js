3b5f619194f6a8b61be397bd9d18cceddf05ced3
Silvia Martinez <sil@silvia.com>
2022-02-25 20:01:34 +0100

    Fixed user model

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   userName: { type: String, required: true },
   password: { type: String, required: true },
+  image: { type: String, required: true },
   friend: [{ type: Schema.Types.ObjectId, ref: "User" }],
   enemie: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
