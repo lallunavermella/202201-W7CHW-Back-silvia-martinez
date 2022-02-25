69166a0ebc300184ee834b350ca53f0f832165e4
Silvia Martinez <sil@silvia.com>
2022-02-25 22:32:42 +0100

    Fixed router, change app to router

--- src/server/routers/userRouters.js
+++ src/server/routers/userRouters.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const app = require("..");
 const userRegister = require("../controllers/userControllers");
 
 const router = express();
 
-app.post("/register", userRegister);
+router.post("/register", userRegister);
 
 module.exports = router;
