9e274ece0ab03116e1dbf698369f3ac6206a09be
Silvia Martinez <sil@silvia.com>
2022-02-25 20:17:10 +0100

    Added middleware errors at server/index

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const { errorNotFound, generalError } = require("./middlewares/errors");
 
 const app = express();
+
+app.use(errorNotFound);
+app.use(generalError);
 
 module.exports = app;
