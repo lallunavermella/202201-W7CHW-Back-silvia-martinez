5064f16cec2725db41e778d4eaf48fd2f26280b4
Silvia Martinez <sil@silvia.com>
2022-02-25 21:20:07 +0100

    Added userRouter at server/index

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const userRouter = require("./routers/userRouters");
 const { errorNotFound, generalError } = require("./middlewares/errors");
 
 const app = express();
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/", userRouter);
 
 app.use(errorNotFound);
 app.use(generalError);
