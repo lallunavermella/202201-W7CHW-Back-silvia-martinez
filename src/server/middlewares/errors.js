04e02a4ffc37b8d8255ae199788a987f3bf570da
Silvia Martinez <sil@silvia.com>
2022-02-25 23:09:25 +0100

    Add colors at debug

--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -2,14 +2,14 @@ const chalk = require("chalk");
 const debug = require("debug")("social-network:error:");
 
 const errorNotFound = (req, res) => {
-  debug(chalk.red("error: Not found"));
+  debug(chalk.bold.bgGrey.red("error: Not found"));
   res.status(404);
   res.json({ error: true, message: "Not found" });
 };
 
 // eslint-disable-next-line no-unused-vars
 const generalError = (err, req, res, next) => {
-  debug(chalk.red("error"));
+  debug(chalk.bold.bgGrey.red(`${err.message}`));
   const status = err.status ?? 500;
   const message = err.message ?? "server error";
   res.status(status).json({ error: message });
