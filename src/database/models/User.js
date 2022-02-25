9485163b80b341892e982ce84ffe2ea78f8e76af
Silvia Martinez <sil@silvia.com>
2022-02-25 19:59:36 +0100

    Added User model

---
+++ src/database/models/User.js
@@ -0,0 +1,13 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  userName: { type: String, required: true },
+  password: { type: String, required: true },
+  friend: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  enemie: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
+const User = model("User", UserSchema, "users");
+
+module.exports = User;
