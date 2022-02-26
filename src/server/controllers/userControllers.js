b9c5e094bc17b39591cf9bf3989ce851c36a4441
Silvia Martinez <sil@silvia.com>
2022-02-26 13:25:27 +0100

    Prove to upload images, but don't work now

--- src/server/controllers/userControllers.js
+++ src/server/controllers/userControllers.js
@@ -1,10 +1,13 @@
 const bcrypt = require("bcrypt");
 const debug = require("debug")("social-network:controller:");
 const chalk = require("chalk");
+/* const path = require("path");
+const fs = require("fs"); */
 const User = require("../../database/models/User");
 
 const userRegister = async (req, res, next) => {
   const user = req.body;
+
   try {
     const userName = await User.findOne({ username: user.userName });
     if (!userName) {
@@ -19,6 +22,12 @@ const userRegister = async (req, res, next) => {
     }
     const password = await bcrypt.hash(user.password, 10);
     await User.create({ ...user, password });
+    /* const oldName = path.join("uploads", req.file.filename);
+    const newName = path.join("uploads", req.file.originalname);
+    fs.rename(oldName, newName, async () => {
+      await User.findByIdAndUpdate(user.id, { image: req.file.originalname });
+    }); */
+
     debug(chalk.bold.bgBlue.magenta(`Created new user: ${user.userName}`));
     res.status(201).json({ name: user.name, userName: user.userName });
   } catch (error) {
