1c1194a7bf65ea72fc226eec0795112e5e3fa850
Silvia Martinez <sil@silvia.com>
2022-02-27 12:26:02 +0100

    Added test at userController

--- src/server/controllers/userControllers.test.js
+++ src/server/controllers/userControllers.test.js
@@ -4,7 +4,7 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 const { default: mongoose } = require("mongoose");
 const connectDB = require("../../database");
 const User = require("../../database/models/User");
-const { userRegister } = require("./userControllers");
+const { userRegister, userLogin } = require("./userControllers");
 
 jest.mock("jsonwebtoken", () => ({
   ...jest.requireActual("jsonwebtoken"),
@@ -37,44 +37,74 @@ afterAll(async () => {
   await server.stop();
 });
 
-describe("Given a user register controller", () => {
-  describe("When it receives a request with valid data", () => {
-    test("Then it should call methods status and json of next of res with 201", async () => {
-      const mockStatus = jest.fn().mockReturnThis();
-      const mockJson = jest.fn();
-      const expectedStatus = 201;
-      const res = { status: mockStatus, json: mockJson };
-      const next = null;
-      const req = {
-        body: {
-          name: "joselito",
-          userName: "joselit0",
-          password: "1234",
-        },
-      };
+describe("Given userController", () => {
+  describe("Given a user register controller", () => {
+    describe("When it receives a request with valid data", () => {
+      test("Then it should call methods status and json of next of res with 201", async () => {
+        const mockStatus = jest.fn().mockReturnThis();
+        const mockJson = jest.fn();
+        const expectedStatus = 201;
+        const res = { status: mockStatus, json: mockJson };
+        const next = null;
+        const req = {
+          body: {
+            name: "joselito",
+            userName: "joselit0",
+            password: "1234",
+          },
+        };
 
-      await userRegister(req, res, next);
+        await userRegister(req, res, next);
 
-      expect(mockStatus).toHaveBeenCalledWith(expectedStatus);
-      expect(mockJson);
+        expect(mockStatus).toHaveBeenCalledWith(expectedStatus);
+        expect(mockJson);
+      });
+    });
+    describe("When it receives a request with an existing userName", () => {
+      test("Then it should return a error", async () => {
+        const error = new Error("failed to create user");
+        const next = jest.fn();
+        const req = {
+          body: {
+            name: "joselito",
+            userName: "Pepe",
+            password: "1234",
+          },
+        };
+        User.create = jest.fn().mockRejectedValue(error);
+
+        await userRegister(req, null, next);
+
+        expect(next).toHaveBeenCalledWith(error);
+      });
     });
   });
-  describe("When it receives a request with an existing userName", () => {
-    test("Then it should return a error", async () => {
-      const error = new Error("failed to create user");
-      const next = jest.fn();
-      const req = {
-        body: {
-          name: "joselito",
-          userName: "Pepe",
-          password: "1234",
-        },
-      };
-      User.create = jest.fn().mockRejectedValue(error);
 
-      await userRegister(req, null, next);
+  describe("Given a login user controller", () => {
+    describe("When it recives a request", () => {
+      test("Then if the user exist should receives a token", async () => {
+        const res = {
+          json: jest.fn(),
+        };
+        const req = { body: { username: "Pepe", password: "1234" } };
+        const token = "token";
 
-      expect(next).toHaveBeenCalledWith(error);
+        await userLogin(req, res);
+
+        expect(res.json).toHaveBeenCalledWith({ token });
+      });
+
+      test("Then if the username or the password is wrong it should return an error", async () => {
+        const req = { body: { username: "Pepo", password: "1236" } };
+
+        const next = jest.fn();
+
+        const error = new Error("Incorrect password or username");
+
+        await userLogin(req, null, next);
+
+        expect(next).toHaveBeenCalledWith(error);
+      });
     });
   });
 });
